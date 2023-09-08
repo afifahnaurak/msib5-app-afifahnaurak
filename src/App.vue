0f96afad471daad2d9c3b77dd1e1037425ab8f9b
Afifah Naura Kamilia <afifahnaurak@gmail.com>
2023-09-08 11:54:13 +0700

    slicing-landing-page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import Nav from './components/Nav.vue'
+import Nav from "./components/Nav.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <div class="min-w-full min-h-full">
     <Nav />
     <router-view />
+    <Footer />
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
