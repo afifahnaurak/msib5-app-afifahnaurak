d809ea9573c9cad7cca58f994430cc245b595e27
Afifah Naura Kamilia <afifahnaurak@gmail.com>
2023-09-08 16:15:48 +0700

    slicing-landing-page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,35 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
-import Home from '../pages/home/index.vue'
-import Todos from '../pages/todo/index.vue'
-import About from '../pages/about/index.vue'
-import LandingPage from '../pages/landingpage/index.vue'
+import Todos from "../pages/todo/index.vue";
+import About from "../pages/about/index.vue";
+import LandingPage from "../pages/landingpage/index.vue";
 
 const routes: RouteRecordRaw[] = [
-    {
-        path: '/',
-        component: LandingPage
-    },
-    {
-        path: '/home',
-        component: Home
-    },
-    {
-        path: '/todos',
-        component: Todos
-    },
-    {
-        path: '/about',
-        component: About
-    },
-    {
-        path: '/landing-page',
-        component: LandingPage
-    }
-]
+  {
+    path: "/",
+    component: LandingPage,
+  },
+  {
+    path: "/home",
+    component: LandingPage,
+  },
+  {
+    path: "/todos",
+    component: Todos,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+  {
+    path: "/landing-page",
+    component: LandingPage,
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
